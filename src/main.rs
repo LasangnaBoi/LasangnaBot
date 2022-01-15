98caa96f6d90b5f786c5b81dcaea412a9208c01d
LasangnaBoi <lasangnaboi@gmail.com>
2022-01-15 03:22:14 -0800

    added several new commands, converted to slash commands

--- src/main.rs
+++ src/main.rs
@@ -5,115 +5,171 @@
  */
 
 mod voice;
-
-use std::env;
-use tokio::join;
-use songbird::SerenityInit;
-use serenity::client::Context;
 use serenity::{
     async_trait,
-    client::{Client, EventHandler},
-    framework::{
-        StandardFramework,
-        standard::{
-            Args, CommandResult,
-            macros::{command, group},
+    model::{
+        gateway::Ready,
+        id::GuildId,
+        interactions::{
+            application_command::{
+            ApplicationCommandOptionType, ApplicationCommand},
+            Interaction,
+            InteractionResponseType,
         },
     },
-    model::{channel::Message, gateway::{Ready, Activity}},
-    Result as SerenityResult,
+    prelude::*,
 };
+use songbird::SerenityInit;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    /// On connect
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "ping" => "Hey, I'm alive!".to_string(),
+                "join" => voice::join(&ctx, &command).await,
+                "play" => voice::play(&ctx, &command).await,
+                "skip" => voice::skip(&ctx, &command).await,
+                "stop" => voice::stop(&ctx, &command).await,
+                "playing" => voice::playing(&ctx, &command).await,
+                "queue" => voice::queue(&ctx, &command).await,
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
-        let t1 = ctx.set_activity(Activity::listening("my parents fight"));
-        join!(t1);
+
+        let _guild_id = GuildId(yourtestguildidhere);
+        /*
+        let _commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| {
+                    command.name("ping").description("A ping command")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("join")
+                        .description("join voice channel")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("play")
+                        .description("search youtube for a song")
+                        .create_option(|option| {
+                            option
+                                .name("query")
+                                .description("what to search youtube for")
+                                .kind(ApplicationCommandOptionType::String)
+                                .required(true)
+                            })
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("skip")
+                        .description("skip the current song")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("stop")
+                        .description("stop playing and clear the queue")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("playing")
+                        .description("get info for the current song")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("queue")
+                        .description("get info for the queue")
+                })
+        })
+        .await;
+*/
+        let _global_commands = ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
+            commands
+                .create_application_command(|command| {
+                    command.name("ping").description("A ping command")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("join")
+                        .description("join voice channel")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("play")
+                        .description("search youtube for a song")
+                        .create_option(|option| {
+                            option
+                                .name("query")
+                                .description("what to search youtube for")
+                                .kind(ApplicationCommandOptionType::String)
+                                .required(true)
+                            })
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("skip")
+                        .description("skip the current song")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("stop")
+                        .description("stop playing and clear the queue")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("playing")
+                        .description("get info for the current song")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("queue")
+                        .description("get info for the queue")
+                })
+        })
+
+            .await;
     }
 }
 
 #[tokio::main]
-async fn main()
-{   
-    tracing_subscriber::fmt::init();
-   
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = <yourdiscordtokenhere>;
 
-    let framework = StandardFramework::new()
-        .configure(|c| c
-                   .prefix("!"))
-        .group(&GENERAL_GROUP);
+    // The Application Id is usually the Bot User Id.
+    let application_id: u64 = <yourapplicationidhere>;
 
-    let mut client = Client::builder(&token)
+    // Build our client.
+    let mut client = Client::builder(token)
         .event_handler(Handler)
-        .framework(framework)
         .register_songbird()
+        .application_id(application_id)
         .await
         .expect("Error creating client");
-   
-    tokio::spawn(async move {
-        let _ = client.start().await.map_err(|why| println!("Client ended: {:?}", why));
-    });
-    
-    tokio::signal::ctrl_c().await
-    .expect("not a command");
-    println!("Received Ctrl-C, shutting down.");
-}
 
-#[group]
-#[commands(ping, join, leave, play, skip, stop)]
-pub struct General;
-
-#[command]
-#[only_in(guilds)]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    check_msg(msg.channel_id.say(&ctx.http, "pong!").await);
-    Ok(())
-}
-
-#[command]
-#[only_in(guilds)]
-async fn join(ctx: &Context, msg: &Message) -> CommandResult {
-    voice::join(ctx, msg).await.expect("error joining");
-    Ok(())
-}
-
-#[command]
-#[only_in(guilds)]
-async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
-    voice::leave(ctx, msg).await.expect("error leaving channel"); 
-    Ok(())
-}
-
-#[command]
-#[only_in(guilds)]
-async fn play(ctx: &Context, msg: &Message) -> CommandResult {
-    voice::yt(ctx, msg).await.expect("error finding song");
-    Ok(())
-}
-
-#[command]
-#[only_in(guilds)]
-async fn skip(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    voice::skip(ctx, msg, _args).await.expect("error skipping song");
-    Ok(())
-}
-
-#[command]
-#[only_in(guilds)]
-async fn stop(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    voice::stop(ctx, msg, _args).await.expect("error stopping");
-    Ok(())
-}
-
-/// Checks that a message successfully sent; if not, then logs why to stdout.
-fn check_msg(result: SerenityResult<Message>) {
-    if let Err(why) = result {
-        println!("Error sending message: {:?}", why);
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
     }
 }
--- src/voice.rs
+++ src/voice.rs
@@ -1,98 +1,82 @@
 /*
- * voice.rs, LasangnaBoi 2022
+ * voice.rs, LsangnaBoi 2022
  * voice channel functionality
  */
-
-use crate::check_msg;
 use serenity::{
     client::Context,
-    framework::
-        standard::{
-            Args,
-            CommandResult,
-        },
-    model::{channel::Message, misc::Mentionable},
+    model::interactions::application_command::{ApplicationCommandInteraction, ApplicationCommandInteractionDataOptionValue},
 };
-use songbird::{input::ytdl_search, create_player};
 use serenity::utils::Colour;
+use songbird::model::id::GuildId;
+use songbird::{input::ytdl_search, create_player};
 
 ///join voice channel
-pub async fn join(ctx: &Context, msg: &Message) -> CommandResult {
-    let guild = msg.guild(&ctx.cache).await.unwrap();
-    let guild_id = guild.id;
-
-    let channel_id = guild
-        .voice_states
-        .get(&msg.author.id)
-        .and_then(|voice_state| voice_state.channel_id);
-
-    let connect_to = match channel_id {
-        Some(channel) => channel,
-        None => {
-            check_msg(msg.reply(ctx, "Not in a voice channel").await);
-
-            return Ok(());
-        },
-    };
-
+pub async fn join(ctx: &Context, command: &ApplicationCommandInteraction) -> String {
+    
+    //get cache and user ID
+    let cache = &ctx.cache;
+    let user_id = command.user.id;
+    let guild_id = command.guild_id;
+    
+    //create manager
     let manager = songbird::get(ctx)
         .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
+        .expect("Songbird voice client placed in at initialization");
+    
+    //retrieve the guild from the cache
+    if let Some(guild) = cache.guild(guild_id.unwrap()).await {
 
-    let (_handle_lock, success) = manager.join(guild_id, connect_to).await;
+        //get channel_id
+        let channel_id = guild
+            .voice_states
+            .get(&user_id)
+            .and_then(|voice_state| voice_state.channel_id);
 
-    if let Ok(_channel) = success {
-        check_msg(
-            msg.channel_id
-                .say(&ctx.http, &format!("Joined {}", connect_to.mention()))
-                .await,
-        );
+        let connect_to = match channel_id {
+            Some(channel) => channel,
+            None => {
+                return String::from("You must be in a voice channel to use this command!");
+            },
+        };
 
+        let name = channel_id.unwrap().name(cache).await.unwrap();
+
+        //join voice channel
+        let (_handle_lock, _success) = manager.join(GuildId(guild_id.unwrap().0).0, connect_to)
+            .await;
+
+        //embed
+        let _ = command.channel_id.send_message(&ctx.http, |m| {
+        let colour = Colour::from_rgb(149, 8, 2);
+        assert_eq!(colour.r(), 149);
+        assert_eq!(colour.g(), 8);
+        assert_eq!(colour.b(), 2);
+        assert_eq!(colour.tuple(), (149, 8, 2));
+        m.embed(|e| {
+            e.title(format!("Joined {}", name));
+            e.colour(colour);
+            e
+        });
+            m
+        }).await;
     } else {
-        check_msg(
-            msg.channel_id
-                .say(&ctx.http, "Error joining the channel")
-                .await,
-        );
+        println!("unable to fetch the guild from the cache");
     }
 
-    Ok(())
-}
-
-///leave voice channel
-pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
-    let guild = msg.guild(&ctx.cache).await.unwrap();
-    let guild_id = guild.id;
-
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-    let has_handler = manager.get(guild_id).is_some();
-
-    if has_handler {
-        if let Err(e) = manager.remove(guild_id).await {
-            check_msg(
-                msg.channel_id
-                    .say(&ctx.http, format!("Failed: {:?}", e))
-                    .await,
-            );
-        }
-
-        check_msg(msg.channel_id.say(&ctx.http, "Left voice channel").await);
-    } else {
-        check_msg(msg.reply(ctx, "Not in a voice channel").await);
-    }
-
-    Ok(())
+    //return
+    String::from("joining...")
 }
 
 ///play song from youtube
-pub async fn yt(ctx: &Context, msg: &Message) -> CommandResult {
-    let query = &msg.content[5..];
-    let guild = msg.guild(&ctx.cache).await.unwrap();
-    let guild_id = guild.id;
+pub async fn play(ctx: &Context, command: &ApplicationCommandInteraction) -> String {
+    //get the guild ID, cache, and query
+    let guild_id = command.guild_id;
+    let cache = &ctx.cache;
+    let query = command.data.options.iter().find(|opt| opt.name == "query").expect("expected input");
+    let query = match query.resolved.as_ref().unwrap() {
+        ApplicationCommandInteractionDataOptionValue::String(s) => s,
+        _ => panic!("expected a string"),
+    };
 
     //create manager
     let manager = songbird::get(ctx)
@@ -100,129 +84,255 @@ pub async fn yt(ctx: &Context, msg: &Message) -> CommandResult {
         .expect("songbird error")
         .clone();
 
-    //create audio source
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let mut handler = handler_lock.lock().await;
+    //if the guild is found
+    if let Some(_guild) = cache.guild(guild_id.unwrap()).await {
+        //create audio source
+        if let Some(handler_lock) = manager.get(guild_id.unwrap().0) {
+            let mut handler = handler_lock.lock().await;
 
-        //get source from YouTube
-        let source = match ytdl_search(query).await {
-            Ok(source) => source,
-            Err(why) => {
-                println!("Err starting source: {:?}", why);
+            //get source from YouTube
+            let source = match ytdl_search(query).await {
+                Ok(source) => source,
+                Err(why) => {
+                    println!("Err starting source: {:?}", why);
+                    return String::from("couldn't source anything");
+                },
+            };
 
-                check_msg(msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await);
+            //create embed
+            //title
+            let title = source.metadata.title.as_ref().unwrap();
+            //channel
+            let channel = source.metadata.channel.as_ref().unwrap();
+            //image
+            let thumbnail = source.metadata.thumbnail.as_ref().unwrap();
+            //embed
+            let url = source.metadata.source_url.as_ref().unwrap();
+            //duration
+            let time = source.metadata.duration.as_ref().unwrap();
+            let minutes = time.as_secs()/60;
+            let seconds = time.as_secs()-minutes*60;
+            let duration = format!("{}:{:02}", minutes, seconds);
+            //color
+            let colour = Colour::from_rgb(149, 8, 2);
+            assert_eq!(colour.r(), 149);
+            assert_eq!(colour.g(), 8);
+            assert_eq!(colour.b(), 2);
+            assert_eq!(colour.tuple(), (149, 8, 2));
+            let _ = command.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.title(title);
+                    e.colour(colour);
+                    e.description(channel);
+                    e.field("duration: ", duration, false);
+                    e.thumbnail(thumbnail);
+                    e.url(url);
+                    e
+                });
+                m
+            }).await;
+           
+            //add to queue
+            let (mut audio, _) = create_player(source);
+            audio.set_volume(0.5);
+            handler.enqueue(audio);
 
-                return Ok(());
-            },
-        };
-
-        //create embed
-        //title
-        let title = source.metadata.title.as_ref().unwrap();
-        //channel
-        let channel = source.metadata.channel.as_ref().unwrap();
-        //image
-        let thumbnail = source.metadata.thumbnail.as_ref().unwrap();
-        //embed
-        let url = source.metadata.source_url.as_ref().unwrap();
-        //duration
-        let time = source.metadata.duration.as_ref().unwrap();
-        let minutes = time.as_secs()/60;
-        let seconds = time.as_secs()-minutes*60;
-        let duration = format!("{}:{:02}", minutes, seconds);
-        //color
-        let colour = Colour::from_rgb(149, 8, 2);
-        assert_eq!(colour.r(), 149);
-        assert_eq!(colour.g(), 8);
-        assert_eq!(colour.b(), 2);
-        assert_eq!(colour.tuple(), (149, 8, 2));
-        let _ = msg.channel_id.send_message(&ctx.http, |m| {
-            m.content("Added to queue:");
-            m.embed(|e| {
-                e.title(title);
-                e.colour(colour);
-                e.description(channel);
-                e.field("duration: ", duration, false);
-                e.thumbnail(thumbnail);
-                e.url(url);
-                e
-            });
-            m
-        }).await;
-       
-        //add to queue
-        let (mut audio, _) = create_player(source);
-        audio.set_volume(0.5);
-        handler.enqueue(audio);
-
-    //if not in a voice channel
-    } else { 
-        check_msg(
-            msg.channel_id
-                .say(&ctx.http, "Must be in a voice channel to use that command!")
-                .await,
-        );
+        //if not in a voice channel
+        } else { 
+            return String::from("Must be in a voice channel to use that command!");
+        }
     }
-    Ok(())
+
+    String::from("Adding to queue:")
 }
 
 ///skip the track
-pub async fn skip(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    let guild = msg.guild(&ctx.cache).await.unwrap();
-    let guild_id = guild.id;
+pub async fn skip(ctx: &Context, command: &ApplicationCommandInteraction) -> String {
+    let cache = &ctx.cache;
+    let guild_id = command.guild_id;
+    if let Some(_guild) = cache.guild(guild_id.unwrap()).await {
+        let manager = songbird::get(ctx)
+            .await
+            .expect("Songbird Voice client placed in at initialisation.")
+            .clone();
 
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let handler = handler_lock.lock().await;
-        let queue = handler.queue();
-        let _ = queue.skip();
-
-        check_msg(
-            msg.channel_id
-                .say(
-                    &ctx.http,
-                    format!("Song skipped: {} in queue.", queue.len()),
-                )
-                .await,
-        );
-    } else {
-        check_msg(
-            msg.channel_id
-                .say(&ctx.http, "Must be in a voice channel to use that command!")
-                .await,
-        );
+        if let Some(handler_lock) = manager.get(guild_id.unwrap().0) {
+            let handler = handler_lock.lock().await;
+            let queue = handler.queue();
+            let _ = queue.skip();
+            //embed
+            let queuesize: usize;
+            if handler.queue().is_empty(){
+                queuesize = 1;
+            } else {
+                queuesize = handler.queue().len() - 1;
+            }
+            let title = format!("Song skipped, {} left in queue.", queuesize);
+            let colour = Colour::from_rgb(149, 8, 2);
+            assert_eq!(colour.r(), 149);
+            assert_eq!(colour.g(), 8);
+            assert_eq!(colour.b(), 2);
+            assert_eq!(colour.tuple(), (149, 8, 2));
+            let _ = command.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.title(title);
+                    e.colour(colour);
+                    e
+                });
+                m
+            }).await;
+        } else {
+            return String::from("Must be in a voice channel to use that command!");
+        }
     }
-
-    Ok(())
+    String::from("Skipping song...")
 }
 
 ///stop playing
-pub async fn stop(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    let guild = msg.guild(&ctx.cache).await.unwrap();
-    let guild_id = guild.id;
+pub async fn stop(ctx: &Context, command: &ApplicationCommandInteraction) -> String {
+    let cache = &ctx.cache;
+    let guild_id = command.guild_id;
+    if let Some(_guild) = cache.guild(guild_id.unwrap()).await {
+        let manager = songbird::get(ctx)
+            .await
+            .expect("Songbird Voice client placed in at initialisation.")
+            .clone();
 
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let handler = handler_lock.lock().await;
-        let queue = handler.queue();
-        let _ = queue.stop();
-
-        check_msg(msg.channel_id.say(&ctx.http, "Queue cleared.").await);
-    } else {
-        check_msg(
-            msg.channel_id
-                .say(&ctx.http, "Must be in a voice channel to use that command!")
-                .await,
-        );
+        if let Some(handler_lock) = manager.get(guild_id.unwrap()) {
+            let handler = handler_lock.lock().await;
+            let queue = handler.queue();
+            let _ = queue.stop();
+        } else {
+            return String::from("Must be in a voice channel to use that command!");
+        }
     }
+    //embed
+    let _ = command.channel_id.send_message(&ctx.http, |m| {
+    let colour = Colour::from_rgb(149, 8, 2);
+    assert_eq!(colour.r(), 149);
+    assert_eq!(colour.g(), 8);
+    assert_eq!(colour.b(), 2);
+    assert_eq!(colour.tuple(), (149, 8, 2));
+    m.embed(|e| {
+        e.title(String::from("Stopped playing, the queue has been cleared."));
+        e.colour(colour);
+        e
+    });
+        m
+    }).await;
+    String::from("stopping...")
+}
 
-    Ok(())
+///current song
+pub async fn playing(ctx: &Context, command: &ApplicationCommandInteraction) -> String {
+    let cache = &ctx.cache;
+    let guild_id = command.guild_id;
+    if let Some(_guild) = cache.guild(guild_id.unwrap()).await {
+        let manager = songbird::get(ctx)
+            .await
+            .expect("Songbird Voice client placed in at initialisation.")
+            .clone();
+        
+        //get the queue
+        if let Some(handler_lock) = manager.get(guild_id.unwrap()) {
+            let handler = handler_lock.lock().await;
+            let queue = handler.queue();
+            let song = &queue.current().unwrap().metadata().clone();
+            //create embed
+            //title
+            let title = &song.title.as_ref().unwrap();
+            //channel
+            let channel = &song.channel.as_ref().unwrap();
+            //image
+            let thumbnail = &song.thumbnail.as_ref().unwrap();
+            //embed
+            let url = &song.source_url.as_ref().unwrap();
+            //duration
+            let time = &song.duration.as_ref().unwrap();
+            let minutes = time.as_secs()/60;
+            let seconds = time.as_secs()-minutes*60;
+            let duration = format!("{}:{:02}", minutes, seconds);
+            //color
+            let colour = Colour::from_rgb(149, 8, 2);
+            assert_eq!(colour.r(), 149);
+            assert_eq!(colour.g(), 8);
+            assert_eq!(colour.b(), 2);
+            assert_eq!(colour.tuple(), (149, 8, 2));
+            let _ = command.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.title(title);
+                    e.colour(colour);
+                    e.description(channel);
+                    e.field("duration: ", duration, false);
+                    e.thumbnail(thumbnail);
+                    e.url(url);
+                    e
+                });
+                m
+            }).await;
+        } else {
+            return String::from("You must be in a voice channel to use that command!");
+        }
+    }
+    String::from("Fetching current song...")
+}
+
+///get the queue
+pub async fn queue(ctx: &Context, command: &ApplicationCommandInteraction) -> String {
+    let cache = &ctx.cache;
+    let guild_id = command.guild_id;
+    if let Some(_guild) = cache.guild(guild_id.unwrap()).await {
+        let manager = songbird::get(ctx)
+            .await
+            .expect("Songbird Voice client placed in at initialisation.")
+            .clone();
+
+        if let Some(handler_lock) = manager.get(guild_id.unwrap()) {
+            let handler = handler_lock.lock().await;
+            let queue = handler.queue();
+            
+            if queue.is_empty() {
+                return String::from("The queue is empty!");
+            }
+            //embed
+            let _ = command.channel_id.send_message(&ctx.http, |m| {
+                //embed
+                let i: usize;
+                if queue.len() < 10 {
+                    i = queue.len();
+                } else {
+                    i = 10;
+                }
+                //color
+                let colour = Colour::from_rgb(149, 8, 2);
+                assert_eq!(colour.r(), 149);
+                assert_eq!(colour.g(), 8);
+                assert_eq!(colour.b(), 2);
+                assert_eq!(colour.tuple(), (149, 8, 2));
+                m.embed(|e| {
+                    e.title("queue");
+                    e.title("Current Queue:");
+                    e.description(format!("current size: {}", queue.len()));
+                    e.color(colour);
+                    for i in 0..i {
+                        let song = &queue.current_queue().get(i).unwrap().metadata().clone();
+                        let channel = &song.channel.as_ref().unwrap();
+                        let title = &song.title.as_ref().unwrap();
+                        //duration
+                        let time = &song.duration.as_ref().unwrap();
+                        let minutes = time.as_secs()/60;
+                        let seconds = time.as_secs()-minutes*60;
+                        let duration = format!("{}:{:02}", minutes, seconds);
+                        let arg1 = format!("{}. {} | {}", i+1, title, channel);
+                        e.field(arg1, duration, false);
+                    }
+                    e
+                });
+                m
+            }).await;
+        } else {
+            return String::from("You must be in a voice channel to use that command!");
+        }
+    }
+    String::from("getting queue...")
 }
